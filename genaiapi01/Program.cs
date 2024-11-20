e1710aee1eacd89818aefdb8c19e8fe81f2afbbe
Salvador Arrazola <salvador.arrazola@gmail.com>
2024-11-19 21:27:27 -0600

    Add example for generating an image.

---
+++ genaiapi01/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenAI_API;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var api = new OpenAIAPI("my-openai-api-key");
+
+var request = new OpenAI_API.Images.ImageGenerationRequest
+{
+  Prompt = "A cute kitten in a basket"
+};
+// var request = OpenAI_API.Images.ImageGenerationRequest
+// {
+//   Prompt = "A cute kitten in a basket"
+// };
+
+var response = await api.ImageGenerations.CreateImageAsync(request);
+// var response = api.ImageGenerations.CreateImageAsync(request);
+
+Console.WriteLine("Image URL: {0}", response.Data[0].Url);
+// Console.WriteLine("Image URL: {0}", response.Data[0].Url);
